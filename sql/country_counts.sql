9380d79456a59e4b78f9a72d5ac602be31649f6c
atoney-CSET <62558612+atoney-CSET@users.noreply.github.com>
2022-10-31 14:45:56 -0400

    Update country_counts.sql

--- sql/country_counts.sql
+++ sql/country_counts.sql
@@ -1,3 +1,4 @@
+--count by country with EU-27 as one entity 
 SELECT count(distinct merged_id) as paper_count, 
 CASE WHEN country IN ("Austria", "Italy", "Belgium", "Latvia", "Bulgaria", "Lithuania", "Croatia", "Luxembourg", "Cyprus", "Malta", "Czechia", "Netherlands", "Denmark", "Poland", "Estonia", "Portugal", "Finland", "Romania", "France", "Slovakia", "Germany", "Slovenia", "Greece", "Spain", "Hungary", "Sweden", "Ireland") THEN "EU-27" ELSE country end as country --treat EU-27 as one entity
 FROM `gcp-cset-projects.trusted_ml_research.trustworthy_ai_papers_0927` 
