a1b7b14622cc8dc4d0200f21a97619eab95d7646
atoney-CSET <62558612+atoney-CSET@users.noreply.github.com>
2022-10-31 14:45:35 -0400

    Update ai_papers_bycountry.sql

--- sql/ai_papers_bycountry.sql
+++ sql/ai_papers_bycountry.sql
@@ -1,3 +1,4 @@
+--get AI papers 
 WITH ai_papers AS(SELECT merged_id FROM gcp_cset_links_v2.corpus_merged
 WHERE
     REGEXP_CONTAINS(FORMAT('%t',(LOWER(title_english),
@@ -5,8 +6,8 @@ WHERE
 AND year >=2010 AND year<=2021)
 
 
-
-SELECT count(distinct merged_id) as paper_count, 
+--count by country with EU-27 as one entity 
+SELECT COUNT(DISTINCT merged_id) AS paper_count, 
 CASE WHEN country IN ("Austria", "Italy", "Belgium", "Latvia", "Bulgaria", "Lithuania", "Croatia", "Luxembourg", "Cyprus", "Malta", "Czechia", "Netherlands", "Denmark", "Poland", "Estonia", "Portugal", "Finland", "Romania", "France", "Slovakia", "Germany", "Slovenia", "Greece", "Spain", "Hungary", "Sweden", "Ireland") THEN "EU-27" ELSE country end as country --treat EU-27 as one entity
 FROM `gcp_cset_links_v2.paper_countries_merged`
 INNER JOIN ai_papers USING(merged_id)
