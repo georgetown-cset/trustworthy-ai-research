b392b38c1a3eea06a0bcb36026784096813622e7
atoney-CSET <62558612+atoney-CSET@users.noreply.github.com>
2023-05-12 10:31:23 -0400

    Update trusted_ai_regex.sql

--- sql/trusted_ai_regex.sql
+++ sql/trusted_ai_regex.sql
@@ -1,3 +1,2 @@
-CREATE OR REPLACE FUNCTION trusted_ml_research.trustedML_en_0927 (str STRING) AS(
-REGEXP_CONTAINS(LOWER(str), r"(?i)((artificial intelligence)|(machine learning))") AND
-REGEXP_CONTAINS(LOWER(str), r"(?i)((trust)|(explainable)|(explainability)|(robustness)|(reliable)|(reliability)|(security)|(resilience)|(interpretable)|(interpretability)|(privacy)|(security)|(safety)|(bias)|(fairness)|(accountability)|(transparency))"))
+CREATE OR REPLACE FUNCTION
+  `gcp-cset-projects.trusted_ml_research.trustworthy_keywords`(str STRING) AS(REGEXP_CONTAINS(LOWER(str), r"(?i)((trust)|(trustworthy)|(explainable)|(explainability)|(robustness)|(reliable)|(reliability)|(security)|(resilience)|(interpretable)|(interpretability)|(privacy)|(security)|(safety)|(bias)|(fairness)|(accountability)|(accountable)|(transparency))"))
