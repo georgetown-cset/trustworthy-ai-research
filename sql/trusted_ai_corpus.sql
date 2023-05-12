4367683bc4f11756344bad82fffc455e2d5f4d6e
atoney-CSET <62558612+atoney-CSET@users.noreply.github.com>
2023-05-12 10:33:16 -0400

    Update trusted_ai_corpus.sql

--- sql/trusted_ai_corpus.sql
+++ sql/trusted_ai_corpus.sql
@@ -1,2 +1,34 @@
-SELECT merged_id, title_english, abstract_english, title_foreign, abstract_foreign, year, doctype, source_title FROM gcp_cset_links_v2.corpus_merged
-WHERE trusted_ml_research.trustedML_en_0927(LOWER(title_english)) OR trusted_ml_research.trustedML_en_0927(LOWER(abstract_english))
+WITH
+  ai_pubs AS(
+  SELECT
+    cset_id AS merged_id
+  FROM
+    `article_classification.predictions_2023_01_19`
+  WHERE
+    ai_filtered IS TRUE),
+  trustworthy_pubs AS(
+  SELECT
+    merged_id,
+    title_english,
+    abstract_english,
+    title_foreign,
+    abstract_foreign,
+    year,
+    doctype,
+    source_title
+  FROM
+    gcp_cset_links_v2.corpus_merged
+  INNER JOIN
+    ai_pubs
+  USING
+    (merged_id)
+  WHERE
+    trusted_ml_research.trustworthy_keywords(LOWER(title_english))
+    OR trusted_ml_research.trustworthy_keywords(LOWER(abstract_english)))
+SELECT
+  *
+FROM
+  trustworthy_pubs
+WHERE
+  year >= 2010
+  AND year <= 2021
