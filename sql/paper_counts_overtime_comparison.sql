d054507d60fa4246b1291a6d9160aa46b5f6fa82
atoney-CSET <62558612+atoney-CSET@users.noreply.github.com>
2023-06-28 09:23:10 -0400

    Update paper_counts_overtime_comparison.sql

--- sql/paper_counts_overtime_comparison.sql
+++ sql/paper_counts_overtime_comparison.sql
@@ -1,12 +1,58 @@
---get AI papers
-WITH ai_counts AS(SELECT COUNT(DISTINCT merged_id) as ai_paper_count, year FROM `gcp_cset_links_v2.corpus_merged`
-                  WHERE (REGEXP_CONTAINS(LOWER(title_english), r"(?i)((artificial intelligence)|(machine learning))") 
-                  OR REGEXP_CONTAINS(LOWER(abstract_english), r"(?i)((artificial intelligence)|(machine learning))")) AND year >= 2010 AND year <= 2021
-                  GROUP BY year),
---get Trustworthy AI papers 
-trusted_counts AS(SELECT COUNT(DISTINCT merged_id) as trusted_count, year FROM `gcp-cset-projects.trusted_ml_research.trustworthy_ai_papers_0927`
-                  WHERE year >= 2010 AND year <= 2021
-                  GROUP BY year )
+WITH
+  --get AI pubs
+  ai_pubs AS(
+  SELECT
+    cset_id AS merged_id,
+    year
+  FROM
+    `article_classification.predictions_2023_01_19`
+  WHERE
+    ai_filtered IS TRUE
+    AND year >= 2010
+    AND year <= 2021),
+  --get Trustworthy AI papers
+  trustworthy_pubs AS(
+  SELECT
+    merged_id,
+    year
+  FROM
+    gcp_cset_links_v2.corpus_merged
+  INNER JOIN
+    ai_pubs
+  USING
+    (merged_id,
+      year)
+  WHERE
+    year >= 2010
+    AND year <=2021
+    AND (trusted_ml_research.trustworthy_keywords(LOWER(title_english))
+      OR trusted_ml_research.trustworthy_keywords(LOWER(abstract_english)))),
+  ai_counts AS(
+  SELECT
+    COUNT(DISTINCT merged_id) AS n_ai,
+    year
+  FROM
+    ai_pubs
+  GROUP BY
+    year),
+  trustworthy_counts AS(
+  SELECT
+    COUNT(DISTINCT merged_id) AS n_trust,
+    year
+  FROM
+    trustworthy_pubs
+  GROUP BY
+    year)
+SELECT
+  *
+FROM
+  ai_counts
+JOIN
+  trustworthy_counts
+USING
+  (year)
 
-SELECT * from ai_counts
-JOIN trusted_counts USING(year)
+
+
+
+  
